2f7f48f4091d11e5bcd2f8be96640def784621bb
wunderfisch <wunderfisch@posteo.de>
2023-08-07 10:54:46 +0200

    small renamings

--- src/components/Questionblock.tsx
+++ src/components/Questionblock.tsx
@@ -10,11 +10,11 @@ export default function Questionblock({ cssquestion }: Props) {
   console.log("id :>> ", cssquestion);
   const { id, question, answer } = cssquestion;
   return (
-    <div class="border px-3 py-4 ">
+    <div className="border px-3 py-4 ">
       <div>
         Question {id}: {question}
       </div>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">
         relveal answer
       </button>
       <div>Answer: {answer}</div>
--- src/pages/api/staticdata.js
+++ src/pages/api/staticdata.js
@@ -4,7 +4,10 @@ import { promises as fs } from "fs";
 export default async function handler(req, res) {
   try {
     const jsonDirectory = path.join(process.cwd(), "data");
-    const fileContents = await fs.readFile(jsonDirectory + "/css.json", "utf8");
+    const fileContents = await fs.readFile(
+      jsonDirectory + "/cssquestions.json",
+      "utf8"
+    );
     // console.log("fileContents :>> ", fileContents);
     // parse the data on the server side
     const parsedata = JSON.parse(fileContents);
