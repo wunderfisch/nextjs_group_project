cc980a8ce761d7124bc5acd0cebb89672d7e8f4c
wunderfisch <wunderfisch@posteo.de>
2023-08-08 16:13:17 +0200

    reveal answers only after button click + don't show button when answer is visible

--- src/components/Questionblock.tsx
+++ src/components/Questionblock.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 type Props = {
   questionx: { id: number; question: string; answer: string };
 };
 
 export default function Questionblock({ questionx }: Props) {
+  const [answerVisible, setAnswerVisible] = useState<boolean>(false);
+
+  const revealAnswer = () => {
+    setAnswerVisible(true);
+  };
   // console.log("id :>> ", question);
   const { id, question, answer } = questionx;
   return (
@@ -12,10 +17,16 @@ export default function Questionblock({ questionx }: Props) {
       <div>
         Question {id}: {question}
       </div>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">
-        relveal answer
-      </button>
-      <div>Answer: {answer}</div>
+      {!answerVisible && (
+        <button
+          onClick={revealAnswer}
+          className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
+        >
+          reveal answer
+        </button>
+      )}
+
+      {answerVisible && <div>Answer: {answer}</div>}
     </div>
   );
 }
