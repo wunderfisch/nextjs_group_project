bff630f4f66be98b516b85fd4b76dedf518e6d28
wunderfisch <wunderfisch@posteo.de>
2023-08-14 18:12:24 +0200

    adding advanced react link to navbar + thoughts on static site generation for fetching components

--- src/pages/techskills/[section].tsx
+++ src/pages/techskills/[section].tsx
@@ -2,6 +2,13 @@ import React from "react";
 import FetchData from "@/components/FetchData";
 import { useRouter } from "next/router";
 
+// // this page doesn't receive props, only later they will be fetched
+// export const getStaticProps:GetStaticProps<
+// // some promise
+// > =async (params:type) => {
+//   // no fetch here
+// }
+
 export default function Section() {
   const params = useRouter();
 
