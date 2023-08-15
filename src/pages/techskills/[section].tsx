3c27200f01bbe9d1400764e061ada042e0e9f206
wunderfisch <wunderfisch@posteo.de>
2023-08-15 15:58:04 +0200

    push before rebuilding. until now setting up StaticProps but not working. still fetching in fetchData component.

--- src/pages/techskills/[section].tsx
+++ src/pages/techskills/[section].tsx
@@ -1,13 +1,49 @@
 import React from "react";
 import FetchData from "@/components/FetchData";
 import { useRouter } from "next/router";
+import { GetStaticProps, GetStaticPaths } from "next";
 
-// // this page doesn't receive props, only later they will be fetched
-// export const getStaticProps:GetStaticProps<
-// // some promise
-// > =async (params:type) => {
-//   // no fetch here
-// }
+interface PageType {
+  // only sting for urlquery or array from the fetch?
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prefetchedPages = [
+    "html",
+    "css",
+    "javascript",
+    "react",
+    "advanced-react",
+  ];
+  const paths = prefetchedPages.map((prefetchedPage) => {
+    return { params: { section: prefetchedPage } };
+  });
+  // console.log("paths :>> ", paths);
+  return {
+    paths: [paths],
+    fallback: false,
+  };
+};
+
+// this page doesn't receive props, only later they will be fetched
+export const getStaticProps: GetStaticProps<PageType> = async (context) => {
+  // no fetch in this component, only render other component
+
+  // // Rauls code:
+  //   // console.log("context :>> ", context);
+  //   console.log("getProps");
+  //   const productId = context.params?.productId;
+
+  //   const response = await fetch(
+  //     `https://fakestoreapi.com/products/${context}`
+  //   );
+  //   const result: ProductType = await response.json();
+  //   //   console.log("result :>> ", result);
+
+  return {
+    // props: what?
+  };
+};
 
 export default function Section() {
   const params = useRouter();
