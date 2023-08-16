74b4249147cd95c2d367217aebd44004c447bc8b
wunderfisch <wunderfisch@posteo.de>
2023-08-16 14:06:06 +0200

    static site generation working in techskill components

--- src/pages/api/staticdata.js
+++ src/pages/api/staticdata.js
@@ -2,21 +2,24 @@ import path from "path";
 import { promises as fs } from "fs";
 
 export default async function handler(req, res) {
-  console.log("req :>> ", req.body.language);
-  console.log("req :>> ", req);
+  //  console.log("req :>> ", req.body.language);
+  // console.log("req in staticdata :>> ", req.query.language);
+  const language = req.query.language;
+
   try {
     const jsonDirectory = path.join(process.cwd(), "data");
+
     const fileContents = await fs.readFile(
       // instead of hardcoded path to json file, url parameter decides the path
       //  jsonDirectory +  `${pathname}questions.json`,
       // jsonDirectory + "/cssquestions.json",
-      jsonDirectory + `/${req.body.language}questions.json`,
+      jsonDirectory + `/${req.query.language}questions.json`,
       "utf8"
     );
     // console.log("fileContents :>> ", fileContents);
     // parse the data on the server side
     const parsedata = JSON.parse(fileContents);
-    console.log("parsedata :>> ", parsedata);
+    // console.log("parsedata :>> ", parsedata);
     res.status(200).json(parsedata);
   } catch (error) {
     console.log("error :>> ", error);
